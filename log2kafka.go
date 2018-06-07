dc64b0bf4cc9bdcaf4a6528dafbd10ea1d5d0ed3
Spiros Ioannou <sivann@gmail.com>
2018-06-07 12:51:21 +0300

    monitor inode to determine file validity

--- log2kafka.go
+++ log2kafka.go
@@ -17,6 +17,7 @@ import (
 	"log/syslog"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -25,8 +26,8 @@ import (
 	"github.com/hpcloud/tail"
 )
 
-var kafkaFlushTimeout = 5000  //message produce timeout (ms)
-var kafkaFlushMaxPending = 10 //with that many undelivered messages, exit
+var kafkaFlushTimeout = 5000 //message produce timeout (ms), override with env KAFKA_FLUSH_TIMEOUT
+var kafkaMaxPending = 10     //with that many undelivered messages, exit. Override with env KAFKA_MAX_PENDING
 
 //KMessage format sent to kafka
 type KMessage struct {
@@ -45,6 +46,7 @@ type KPayload struct {
 	FTPCommand      string
 	FTPResponse     string
 	FileOffset      int64
+	FileInode       uint64
 }
 
 func check(e error) {
@@ -62,42 +64,66 @@ func hashString(s string) string {
 
 // Saves position to state.dat file so we can continue reading where we left off if restarted
 // Called only after data successfully sent to kafka
-func savePos(offset int64, line string) {
-	f, err := os.Create("state.dat")
+func savePos(offset int64, inode uint64, line string, stateFile string) {
+	f, err := os.Create(stateFile)
 	check(err)
 	defer f.Close()
 	check(err)
 
-	s := fmt.Sprintf("%d %s\n", offset, hashString(line))
+	log.Printf("savePos: saving to %s offset:%d inode:%d line:%s\n", stateFile, offset, inode, line)
+
+	s := fmt.Sprintf("%d %d %s\n", offset, inode, line)
 	_, err = f.WriteString(s)
 	check(err)
 	f.Sync()
 }
 
 //called on start, seek to previously stored logfile position
-func readPos() (bool, int64, string) {
+func readPos(stateFile string) (bool, int64, uint64, string) {
 	var offset int64
-	var hash string
+	var inode uint64
+	var str string
 	var ok bool
 
-	f, err := os.Open("state.dat")
+	f, err := os.Open(stateFile)
 	if err != nil {
-		return false, 0, ""
+		return false, 0, 0, ""
 	}
 	defer f.Close()
-	n, err := fmt.Fscanf(f, "%d %s\n", &offset, &hash)
+	n, err := fmt.Fscanf(f, "%d %d %s\n", &offset, &inode, &str)
 
 	ok = false
-	if (n == 2) && (err == nil) {
+	if (n == 3) && (err == nil) {
 		ok = true
 	}
 
 	//fmt.Printf("GOT %d items: %d and %s, err:%v, ok:%v\n", n, offset, hash, err, ok)
-	return ok, offset, hash
+	return ok, offset, inode, str
+}
+
+// Used when starting to determine seek position.
+// If ftp logfile is the same (checks inode), and state file is valid: last saved position. Else seek to start.
+func determineLogOffset(stateFile string, ftpLogFile string) *tail.SeekInfo {
+	stateFileOk, offset, savedInode, fn := readPos(stateFile)
+	log.Printf("Read previous position:state:%v offset:%d, inode:%d, file:%s\n", stateFileOk, offset, savedInode, fn)
+
+	currentInode := getInodeOfFile(ftpLogFile)
+
+	var seekInfo *tail.SeekInfo
+	if stateFileOk && currentInode == savedInode {
+		log.Printf("ftp logfile inodes match (%d) and statefile is valid: seeking to offset:%d\n", currentInode, offset)
+		seekInfo = &tail.SeekInfo{Offset: offset, Whence: io.SeekStart}
+	} else if !stateFileOk {
+		log.Printf("WARNING: statefile seems corrupted, seeking to start of logfile")
+		seekInfo = &tail.SeekInfo{Offset: 0, Whence: io.SeekStart}
+	} else {
+		log.Printf("WARNING: logfile seems changed since we last tailed it (previous inode: %d, current inode:%d), seeking to start of logfile.", savedInode, currentInode)
+	}
+	return seekInfo
 }
 
 //get an xferlog line and create a kafka json message
-func xferlog2KMessage(line string, offset int64) ([]byte, error) {
+func xferlog2KMessage(line string, offset int64, inode uint64) ([]byte, error) {
 	//Example line from ftp:
 	//2018-06-06T13:24:56|195.46.28.226|ftp-admin|/bbb/LibreOffice_6.0.4_MacOS_x86-64.dmg|91488256||STOR LibreOffice_6.0.4_MacOS_x86-64.dmg|226
 
@@ -118,6 +144,7 @@ func xferlog2KMessage(line string, offset int64) ([]byte, error) {
 			FTPCommand:      s[6],
 			FTPResponse:     s[7],
 			FileOffset:      offset,
+			FileInode:       inode,
 		},
 	}
 	jdata, _ := json.Marshal(m)
@@ -125,10 +152,16 @@ func xferlog2KMessage(line string, offset int64) ([]byte, error) {
 	return jdata, nil
 }
 
+func getInodeOfFile(fileName string) uint64 {
+	fileinfo, _ := os.Stat(fileName)
+	stat, _ := fileinfo.Sys().(*syscall.Stat_t)
+	return stat.Ino
+}
+
 //Monitor logfile and on each new valid line call kafkaSend
-func tailFile(path string, cfg tail.Config,
+func tailFile(logFile string, cfg tail.Config,
 	td chan *tail.Tail, producer *kafka.Producer, topic string) {
-	t, err := tail.TailFile(path, cfg)
+	t, err := tail.TailFile(logFile, cfg)
 	defer t.Cleanup()
 	td <- t
 
@@ -139,8 +172,9 @@ func tailFile(path string, cfg tail.Config,
 	i := 0
 	for line := range t.Lines {
 		i++
-		offset, err := t.Tell()                           //current offset
-		jdata, err := xferlog2KMessage(line.Text, offset) //pass current offset so producer callback knows where to save
+		offset, err := t.Tell() //current offset
+		inode := getInodeOfFile(logFile)
+		jdata, err := xferlog2KMessage(line.Text, offset, inode) //pass current offset and inode so producer callback knows where to save
 		if err == nil {
 			kafkaSend(producer, topic, jdata)
 		} else {
@@ -159,7 +193,7 @@ func sigHdl(sigc <-chan os.Signal) {
 }
 
 // Callback from kafka producer. If kafka produce was a success, save logfile position of that message. We assume in-order.
-func kafkaProdEvtMon(producer *kafka.Producer) {
+func kafkaProdEvtMon(producer *kafka.Producer, stateFile string) {
 	log.Printf("kafkaProdEvtMon:Started\n")
 	for e := range producer.Events() {
 		switch ev := e.(type) {
@@ -171,17 +205,19 @@ func kafkaProdEvtMon(producer *kafka.Producer) {
 			} else {
 				km := new(KMessage)
 				err := json.Unmarshal(m.Value, &km)
-				var fn string
+				var ftpFileName string
 				var offset int64
+				var inode uint64
 				if err == nil {
 					offset = km.KPayload.FileOffset
-					fn = km.KPayload.Filename
-					//fmt.Printf("OFFSET:%v %s\n", km.KPayload.FileOffset, km.KPayload.Filename)
-					savePos(offset, fn)
+					ftpFileName = km.KPayload.Filename
+					inode = km.KPayload.FileInode
+
+					savePos(offset, inode, ftpFileName, stateFile)
 				}
-				log.Printf("kafkaProdEvtMon:Delivered message to topic %s [%d] at offset %v, filename: %s\n",
+				log.Printf("kafkaProdEvtMon:Delivered message to topic: %s [%d] kafka_offset: %v, filename: %s\n",
 					*m.TopicPartition.Topic, m.TopicPartition.Partition,
-					m.TopicPartition.Offset, fn)
+					m.TopicPartition.Offset, ftpFileName)
 			}
 
 		default:
@@ -206,7 +242,7 @@ func kafkaSend(producer *kafka.Producer, topic string, value []byte) {
 	if npending > 1 {
 		log.Printf("kafkaSend:ERROR: %d unflushed messages\n", npending)
 	}
-	if npending > kafkaFlushMaxPending {
+	if npending > kafkaMaxPending {
 		log.Printf("kafkaSend:ERROR: %d unflushed messages, exiting\n", npending)
 		os.Exit(1)
 	}
@@ -221,14 +257,30 @@ func logSetup() {
 
 }
 
+func init() {
+	var x string
+	x = os.Getenv("KAFKA_FLUSH_TIMEOUT")
+	if len(x) > 0 {
+		kafkaFlushTimeout, _ = strconv.Atoi(x)
+	}
+	x = os.Getenv("KAFKA_MAX_PENDING")
+	if len(x) > 0 {
+		kafkaMaxPending, _ = strconv.Atoi(x)
+	}
+	log.Printf("Initialized with kafkaFlushTimeout:%d, kafkaMaxPending:%d\n", kafkaFlushTimeout, kafkaMaxPending)
+}
+
 func main() {
 	var offset int64
-	var broker = flag.String("broker", "", "kafka bootstrap servers, e.g.: localhost:9092")
-	var topic = flag.String("topic", "", "topic name")
+	var broker = flag.String("b", "", "kafka `bootstrap servers`, e.g.: localhost:9092")
+	var topic = flag.String("t", "", "`topic` name")
+	var ftpLogFile = flag.String("f", "", "ftp `logfile`")
+	var stateFile = flag.String("s", "", "`statefile`, a file where to save our state")
+
 	flag.Parse()
 
-	if len(os.Args) != 5 {
-		fmt.Fprintf(os.Stderr, "Usage: %s -broker <broker> -topic <topic>\n",
+	if len(os.Args) != 9 {
+		fmt.Fprintf(os.Stderr, "Usage: %s -s <statefile> -f <logfile> -b <broker> -t <topic>\n",
 			os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -240,9 +292,9 @@ func main() {
 	producer, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": *broker,
 		"acks":              1,
-		"retries":           2,
-		//"max.block.ms":                          10000, //60000
-		"request.timeout.ms":                    5000, //30000
+		"retries":           3,
+		//"max.block.ms":                        10000, //60000
+		"request.timeout.ms":                    15000, //30000
 		"max.in.flight.requests.per.connection": 1,
 		"produce.offset.report":                 true,
 		"auto.offset.reset":                     "latest",
@@ -255,41 +307,31 @@ func main() {
 	log.Printf("Created Producer %v (%s)\n", producer, verstr)
 
 	//Start the Kafka event monitor goroutine
-	go kafkaProdEvtMon(producer)
+	go kafkaProdEvtMon(producer, *stateFile)
 
-	td := make(chan *tail.Tail) //used to communicate the *tail
+	td := make(chan *tail.Tail) //used to communicate the *tail descriptor
 	sigs := make(chan os.Signal, 5)
 
 	signal.Notify(sigs, syscall.SIGINT)
 	go sigHdl(sigs)
 
-	//Seek to previously saved logfile position
-	stateOk, offset, fn := readPos()
-	log.Printf("Read previous position:%v \toffset:%d, file:%s\n", stateOk, offset, fn)
-	var seekInfo *tail.SeekInfo
-	if stateOk {
-		log.Printf("Seeking to offset:%d\n", offset)
-		seekInfo = &tail.SeekInfo{Offset: offset, Whence: io.SeekStart}
-	} else {
-		seekInfo = &tail.SeekInfo{Offset: 0, Whence: io.SeekStart}
-	}
+	//Find appropriate file location from previously saved logfile position
+	seekInfo := determineLogOffset(*stateFile, *ftpLogFile)
 
-	//
 	cfg := tail.Config{
 		Follow:   true,
 		ReOpen:   true,
 		Location: seekInfo,
 	}
-
 	// Start the logfile monitor goroutine
-	go tailFile("l1.log", cfg, td, producer, *topic)
+	go tailFile(*ftpLogFile, cfg, td, producer, *topic)
 	t1 := <-td //wait for it to start and get the *tail so we can access file offset (t1.Tell()) from elsewhere
 
 	_ = err
 
 	for {
 		offset, err = t1.Tell()
-		log.Printf("ftell offset: %v, Events length:%d, Producer length:%d\n", offset, len(producer.Events()), len(producer.ProduceChannel()))
+		log.Printf("log offset: %v, EventsLen:%d, ProducerLen:%d\n", offset, len(producer.Events()), len(producer.ProduceChannel()))
 		time.Sleep(5 * time.Second)
 	}
 
--- x
+++
@@ -1,3 +0,0 @@
-%3|1528270208.663|FAIL|rdkafka#producer-1| [thrd:xxx.com:9092/bootstrap]: xxx.com:9092/bootstrap: Connect to ipv4#xxx.xxx.x.40:9092 failed: Connection refused
-%3|1528270208.663|ERROR|rdkafka#producer-1| [thrd:xxx.com:9092/bootstrap]: xxx.com:9092/bootstrap: Connect to ipv4#xxx.xxx.x.40:9092 failed: Connection refused
-%3|1528270208.663|ERROR|rdkafka#producer-1| [thrd:xxx.com:9092/bootstrap]: 1/1 brokers are down
